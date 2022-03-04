acb1e82540d385fee549c4e36e436172a495b0b7
matheusfillipe <matheusfillipeag@gmail.com>
2022-03-04 02:47:09 -0300

    Minimal CLI

--- cleverbot/__main__.py
+++ cleverbot/__main__.py
@@ -1,17 +1,43 @@
+from sys import argv
+
 from .cleverbot import Cleverbot
 
-alice = Cleverbot()
-bob = Cleverbot()
 
-message = "Hello there, bitch"
-
-if __name__ == "__main__":
+def chat():
+    """
+    Chat with Cleverbot.
+    """
+    bot = Cleverbot()
     print("Start the conversation, press Ctrl-c to stop \n")
     try:
         while True:
-            message = alice.post_message(message)
-            print("Alice: ", message)
-            message = bob.post_message(message)
-            print("Bob: ", message)
+            print(bot.send(input(">> ")))
     except KeyboardInterrupt:
         print("Exiting.")
+
+
+def self_chat():
+    """
+    This function is used to chat with the bot.
+    """
+    alice = Cleverbot()
+    bob = Cleverbot()
+    message = "Hi there! How are you doing?"
+    print("Press Ctrl-c to stop \n")
+    try:
+        while True:
+            print("Bob: ", message)
+            message = alice.send(message)
+            print("Alice: ", message)
+            message = bob.send(message)
+    except KeyboardInterrupt:
+        print("Exiting.")
+
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        chat()
+    elif len(argv) == 2 and argv[1] == "auto":
+        self_chat()
+    else:
+        print("Usage: python -m cleverbot [auto]")
